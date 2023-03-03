070707aa1fa169c034e9e3763a140497f5ca6ec3
kontorol <60671553+kontorol@users.noreply.github.com>
2023-03-03 22:33:37 +0000

    EN TRANSLATE

--- main.go
+++ main.go
@@ -21,86 +21,86 @@ func init() {
 	var printVersion bool
 	var help = `
 CloudflareSpeedTest ` + version + `
-测试 Cloudflare CDN 所有 IP 的延迟和速度，获取最快 IP (IPv4+IPv6)！
+Test the latency and speed of all IPs of Cloudflare CDN, get the fastest IP (IPv4+IPv6)!
 https://github.com/XIU2/CloudflareSpeedTest
 
-参数：
+parameter:
     -n 200
-        延迟测速线程；越多延迟测速越快，性能弱的设备 (如路由器) 请勿太高；(默认 200 最多 1000)
+        Delay speed measurement thread; the more delays, the faster the speed measurement, and devices with weak performance (such as routers) should not be too high; (default 200, maximum 1000)
     -t 4
-        延迟测速次数；单个 IP 延迟测速次数，为 1 时将过滤丢包的IP；(默认 4 次)
+        The number of delayed speed tests; the number of delayed speed tests for a single IP, when it is 1, the IP with packet loss will be filtered; (default 4 times)
     -dn 10
-        下载测速数量；延迟测速并排序后，从最低延迟起下载测速的数量；(默认 10 个)
+        Number of download speed tests; after delay and sorting, the number of download speed tests from the lowest delay; (default 10)
     -dt 10
-        下载测速时间；单个 IP 下载测速最长时间，不能太短；(默认 10 秒)
+        Download speed test time; the maximum time for a single IP download speed test, not too short; (default 10 seconds)
     -tp 443
-        指定测速端口；延迟测速/下载测速时使用的端口；(默认 443 端口)
+        Specify the speed test port; the port used for delay speed test/download speed test; (default port 443)
     -url https://cf.xiu2.xyz/url
-        指定测速地址；延迟测速(HTTPing)/下载测速时使用的地址，默认地址不保证可用性，建议自建；
+        Specify the speed test address; the address used for delayed speed test (HTTPing)/download speed test. The default address does not guarantee availability, and it is recommended to build it yourself;
 
     -httping
-        切换测速模式；延迟测速模式改为 HTTP 协议，所用测试地址为 [-url] 参数；(默认 TCPing)
+        Switch the speed measurement mode; change the delayed speed measurement mode to the HTTP protocol, and the test address used is the [-url] parameter; (default TCPing)
     -httping-code 200
-        有效状态代码；HTTPing 延迟测速时网页返回的有效 HTTP 状态码，仅限一个；(默认 200 301 302)
+        Valid status code; only one valid HTTP status code returned by the web page during HTTPing delay speed test; (default 200 301 302)
     -cfcolo HKG,KHH,NRT,LAX,SEA,SJC,FRA,MAD
-        匹配指定地区；地区名为当地机场三字码，英文逗号分隔，仅 HTTPing 模式可用；(默认 所有地区)
+        Match the specified region; the region name is the three-character code of the local airport, separated by English commas, only available in HTTPing mode; (default all regions)
 
     -tl 200
-        平均延迟上限；只输出低于指定平均延迟的 IP，可与其他上限/下限搭配；(默认 9999 ms)
+        Upper limit of average delay; only output IPs lower than the specified average delay, which can be matched with other upper/lower limits; (default 9999 ms)
     -tll 40
-        平均延迟下限；只输出高于指定平均延迟的 IP，可与其他上限/下限搭配；(默认 0 ms)
+        Average delay lower limit; only output IPs higher than the specified average delay, can be used with other upper/lower limits; (default 0 ms)
     -sl 5
-        下载速度下限；只输出高于指定下载速度的 IP，凑够指定数量 [-dn] 才会停止测速；(默认 0.00 MB/s)
+        Download speed lower limit; only output IPs higher than the specified download speed, and the speed measurement will stop when the specified number [-dn] is reached; (default 0.00 MB/s)
 
     -p 10
-        显示结果数量；测速后直接显示指定数量的结果，为 0 时不显示结果直接退出；(默认 10 个)
+        Display the number of results; directly display the specified number of results after the speed measurement, and exit without displaying the results when it is 0; (default 10)
     -f ip.txt
-        IP段数据文件；如路径含有空格请加上引号；支持其他 CDN IP段；(默认 ip.txt)
+        IP segment data file; if the path contains spaces, please add quotation marks; support other CDN IP segments; (default ip.txt)
     -ip 1.1.1.1,2.2.2.2/24,2606:4700::/32
-        指定IP段数据；直接通过参数指定要测速的 IP 段数据，英文逗号分隔；(默认 空)
+        Specify the IP segment data; directly specify the IP segment data to be tested by parameters, separated by English commas; (default empty)
     -o result.csv
-        写入结果文件；如路径含有空格请加上引号；值为空时不写入文件 [-o ""]；(默认 result.csv)
+        Write the result file; if the path contains spaces, please add quotation marks; if the value is empty, do not write to the file [-o ""]; (default result.csv)
 
     -dd
-        禁用下载测速；禁用后测速结果会按延迟排序 (默认按下载速度排序)；(默认 启用)
+        Disable download speed measurement; after disabling, the speed test results will be sorted by delay (default sorted by download speed); (default enabled)
     -allip
-        测速全部的IP；对 IP 段中的每个 IP (仅支持 IPv4) 进行测速；(默认 每个 /24 段随机测速一个 IP)
+        Speed test all IPs; test the speed of each IP in the IP segment (only supports IPv4); (by default, each /24 segment will randomly test one IP)
 
     -v
-        打印程序版本 + 检查版本更新
+        Print program version + check for version updates
     -h
-        打印帮助说明
+        print help instructions
 `
 	var minDelay, maxDelay, downloadTime int
-	flag.IntVar(&task.Routines, "n", 200, "延迟测速线程")
-	flag.IntVar(&task.PingTimes, "t", 4, "延迟测速次数")
-	flag.IntVar(&task.TestCount, "dn", 10, "下载测速数量")
-	flag.IntVar(&downloadTime, "dt", 10, "下载测速时间")
-	flag.IntVar(&task.TCPPort, "tp", 443, "指定测速端口")
-	flag.StringVar(&task.URL, "url", "https://cf.xiu2.xyz/url", "指定测速地址")
+	flag.IntVar(&task.Routines, "n", 200, "Delay Speed Test Thread")
+	flag.IntVar(&task.PingTimes, "t", 4, "Delay speed test times")
+	flag.IntVar(&task.TestCount, "dn", 10, "Number of download speed tests")
+	flag.IntVar(&downloadTime, "dt", 10, "Download speed test time")
+	flag.IntVar(&task.TCPPort, "tp", 443, "Specified speed test port")
+	flag.StringVar(&task.URL, "url", "https://cf.xiu2.xyz/url", "Specified speed test address")
 
-	flag.BoolVar(&task.Httping, "httping", false, "切换测速模式")
-	flag.IntVar(&task.HttpingStatusCode, "httping-code", 0, "有效状态代码")
-	flag.StringVar(&task.HttpingCFColo, "cfcolo", "", "匹配指定地区")
+	flag.BoolVar(&task.Httping, "httping", false, "Switch speed measurement mode")
+	flag.IntVar(&task.HttpingStatusCode, "httping-code", 0, "valid status code")
+	flag.StringVar(&task.HttpingCFColo, "cfcolo", "", "Match the specified region")
 
-	flag.IntVar(&maxDelay, "tl", 9999, "平均延迟上限")
-	flag.IntVar(&minDelay, "tll", 0, "平均延迟下限")
-	flag.Float64Var(&task.MinSpeed, "sl", 0, "下载速度下限")
+	flag.IntVar(&maxDelay, "tl", 9999, "average delay limit")
+	flag.IntVar(&minDelay, "tll", 0, "lower limit of average delay")
+	flag.Float64Var(&task.MinSpeed, "sl", 0, "Lower limit of download speed")
 
-	flag.IntVar(&utils.PrintNum, "p", 10, "显示结果数量")
-	flag.StringVar(&task.IPFile, "f", "ip.txt", "IP段数据文件")
-	flag.StringVar(&task.IPText, "ip", "", "指定IP段数据")
-	flag.StringVar(&utils.Output, "o", "result.csv", "输出结果文件")
+	flag.IntVar(&utils.PrintNum, "p", 10, "Display the number of results")
+	flag.StringVar(&task.IPFile, "f", "ip.txt", "IP segment data file")
+	flag.StringVar(&task.IPText, "ip", "", "Specified IP segment data")
+	flag.StringVar(&utils.Output, "o", "result.csv", "output result file")
 
-	flag.BoolVar(&task.Disable, "dd", false, "禁用下载测速")
-	flag.BoolVar(&task.TestAll, "allip", false, "测速全部 IP")
+	flag.BoolVar(&task.Disable, "dd", false, "Disable download speed test")
+	flag.BoolVar(&task.TestAll, "allip", false, "Test all IPs")
 
-	flag.BoolVar(&printVersion, "v", false, "打印程序版本")
+	flag.BoolVar(&printVersion, "v", false, "print program version")
 	flag.Usage = func() { fmt.Print(help) }
-	flag.Parse()
+	flag. Parse()
 
 	if task.MinSpeed > 0 && time.Duration(maxDelay)*time.Millisecond == utils.InputMaxDelay {
-		fmt.Println("[小提示] 在使用 [-sl] 参数时，建议搭配 [-tl] 参数，以避免因凑不够 [-dn] 数量而一直测速...")
+		fmt.Println("[Tips] When using the [-sl] parameter, it is recommended to use the [-tl] parameter to avoid constant speed measurement due to insufficient [-dn] numbers...")
 	}
 	utils.InputMaxDelay = time.Duration(maxDelay) * time.Millisecond
 	utils.InputMinDelay = time.Duration(minDelay) * time.Millisecond
@@ -109,31 +109,31 @@ https://github.com/XIU2/CloudflareSpeedTest
 
 	if printVersion {
 		println(version)
-		fmt.Println("检查版本更新中...")
+		fmt.Println("Checking for version updates...")
 		checkUpdate()
 		if versionNew != "" {
-			fmt.Printf("*** 发现新版本 [%s]！请前往 [https://github.com/XIU2/CloudflareSpeedTest] 更新！ ***", versionNew)
+			fmt.Printf("*** Found a new version [%s]! Please go to [https://github.com/XIU2/CloudflareSpeedTest] to update! ***", versionNew)
 		} else {
-			fmt.Println("当前为最新版本 [" + version + "]！")
+			fmt.Println("Currently the latest version [" + version + "]!")
 		}
 		os.Exit(0)
 	}
 }
 
 func main() {
-	task.InitRandSeed() // 置随机数种子
+	task.InitRandSeed() // set random number seed
 
 	fmt.Printf("# XIU2/CloudflareSpeedTest %s \n\n", version)
 
-	// 开始延迟测速
+	// Start delayed speed measurement
 	pingData := task.NewPing().Run().FilterDelay()
-	// 开始下载测速
+	// start download speed test
 	speedData := task.TestDownloadSpeed(pingData)
-	utils.ExportCsv(speedData) // 输出文件
-	speedData.Print()          // 打印结果
+	utils.ExportCsv(speedData) // output file
+	speedData.Print() // print the result
 
 	if versionNew != "" {
-		fmt.Printf("\n*** 发现新版本 [%s]！请前往 [https://github.com/XIU2/CloudflareSpeedTest] 更新！ ***\n", versionNew)
+		fmt.Printf("\n*** Found new version [%s]! Please go to [https://github.com/XIU2/CloudflareSpeedTest] to update! ***\n", versionNew)
 	}
 	endPrint()
 }
@@ -142,13 +142,13 @@ func endPrint() {
 	if utils.NoPrintResult() {
 		return
 	}
-	if runtime.GOOS == "windows" { // 如果是 Windows 系统，则需要按下 回车键 或 Ctrl+C 退出（避免通过双击运行时，测速完毕后直接关闭）
-		fmt.Printf("按下 回车键 或 Ctrl+C 退出。")
+	if runtime.GOOS == "windows" { // If it is a Windows system, you need to press the Enter key or Ctrl+C to exit (avoid double-clicking to run and close directly after the speed measurement is completed)
+		fmt.Printf("Press Enter or Ctrl+C to exit.")
 		fmt.Scanln()
 	}
 }
 
-// 检查更新
+// Check for updates
 func checkUpdate() {
 	timeout := 10 * time.Second
 	client := http.Client{Timeout: timeout}
@@ -156,12 +156,12 @@ func checkUpdate() {
 	if err != nil {
 		return
 	}
-	// 读取资源数据 body: []byte
+	// read resource data body: []byte
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
-	// 关闭资源流
+	// close the resource stream
 	defer res.Body.Close()
 	if string(body) != version {
 		versionNew = string(body)
