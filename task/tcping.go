40c5addd6b087842dd208d6ea22d77d71b47f6b3
kontorol <60671553+kontorol@users.noreply.github.com>
2023-03-03 23:50:43 +0000

    Update tcping.go

--- task/tcping.go
+++ task/tcping.go
@@ -64,9 +64,9 @@ func (p *Ping) Run() utils.PingDelaySet {
 		return p.csv
 	}
 	if Httping {
-		fmt.Printf("开始延迟测速（模式：HTTP，端口：%d，平均延迟上限：%v ms，平均延迟下限：%v ms)\n", TCPPort, utils.InputMaxDelay.Milliseconds(), utils.InputMinDelay.Milliseconds())
+		fmt.Printf("Start latency test (mode: HTTP, port: %d, average latency upper limit: %v ms, average latency lower limit: %v ms)\n", TCPPort, utils.InputMaxDelay.Milliseconds(), utils.InputMinDelay.Milliseconds())
 	} else {
-		fmt.Printf("开始延迟测速（模式：TCP，端口：%d，平均延迟上限：%v ms，平均延迟下限：%v ms)\n", TCPPort, utils.InputMaxDelay.Milliseconds(), utils.InputMinDelay.Milliseconds())
+		fmt.Printf("start latency test(mode: TCP, port: %, average latency upper bound: %v ms, average latency lower bound: %v ms)\n", TCPPort, utils.InputMaxDelay.Milliseconds(), utils.InputMinDelay.Milliseconds())
 	}
 	for _, ip := range p.ips {
 		p.wg.Add(1)
